6775ed3a8e31809b2e6f21738e4f730c4d491b37
jamshaidkhalid <jamshaidkhalid32@gmail.com>
2023-01-16 23:09:26 +0500

    😍

--- index.py
+++ index.py
@@ -28,16 +28,25 @@ def navigateToHome():
 
 def launchBrowser():
     wait = WebDriverWait(driver, 10)
+
+    #targeting username and password fields
     username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
     password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
 
+    #clearing the fields and filling them with the data from the inquirer
     username.clear()
     username.send_keys(inq.answers['username'])
 
     password.clear()
     password.send_keys(inq.answers['password'])
 
+
+    #targeting the login button and clicking it
     Login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+
+    #targeting the not now button on the save login info popup and clicking it
+    not_now = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+    not_now2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
 
     
     ex.wb.save("sample.xlsx")
